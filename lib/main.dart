7e8f71f24488092e2ccafbf6c6e53fcbc30ed205
qoranreh <qoranreh@gmail.com>
2024-09-01 18:38:44 +0900

    error_server_data

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -19,11 +21,38 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
+
+
 //변수 넣는 곳
 var tap =0;
 var images = ['assets/images.jpeg','assets/images (1).jpeg','assets/Instagram1.jpeg'];
 var imagesNum=0;
+var likes=[];
+var user =[];
+var content =[];
 class _MyAppState extends State<MyApp> {
+
+  getData()async{
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    //링크 변수에 넣어쓰기
+    var result2=jsonDecode(result.body);
+    print(result2[0]);
+    for(int i=0;i<3;i++)
+      {
+        likes.add(result2[i]['likes']);
+        content.add(result2[i]['content']);
+        user.add(result2[i]['user']);
+      }
+    print(likes);
+  }
+
+  @override
+  void initState(){
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +62,7 @@ class _MyAppState extends State<MyApp> {
         actions: [
           IconButton(onPressed: (){}, icon: Icon(Icons.add_box_outlined,))],
       ),
-      body: [homeMain(),Text('샵페이지')][tap]
+      body: [homeMain(likes:likes, content: content,user:user),Text('샵페이지')][tap]
       ,
       bottomNavigationBar: BottomNavigationBar(
         showSelectedLabels: false,
@@ -52,8 +81,10 @@ class _MyAppState extends State<MyApp> {
   }
 }
 class homeMain extends StatelessWidget {
-  const homeMain({super.key});
-
+   homeMain({super.key,this.likes,this.content,this.user});
+  var likes;
+  var content;
+  var user;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -75,9 +106,9 @@ class homeMain extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('좋아요 100'),
-                        Text('글쓴이'),
-                        Text('글내용')],
+                        Text(likes[i].toString()),
+                        Text(user[i]),
+                        Text(content[i])],
                     ),
                   )
                 ]),
